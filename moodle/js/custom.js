16c8b00c086ddb529bc5d15cd69db298a0f78443
michaelolknight <michaelolknight92@gmail.com>
2022-03-04 15:34:55 +0000

    Add files via upload

---
+++ moodle/js/custom.js
@@ -0,0 +1,161 @@
+/* JS Document */
+
+/******************************
+
+[Table of Contents]
+
+1. Vars and Inits
+2. Set Header
+3. Init Menu
+4. Init Home Slider
+5. Init Testimonials Slider
+
+
+******************************/
+
+$(document).ready(function()
+{
+	"use strict";
+
+	/* 
+
+	1. Vars and Inits
+
+	*/
+
+	var header = $('.header');
+	var ctrl = new ScrollMagic.Controller();
+
+	setHeader();
+
+	$(window).on('resize', function()
+	{
+		setHeader();
+	});
+
+	$(document).on('scroll', function()
+	{
+		setHeader();
+	});
+
+	initMenu();
+	initHomeSlider();
+	initTestSlider();
+
+	/* 
+
+	2. Set Header
+
+	*/
+
+	function setHeader()
+	{
+		if($(window).scrollTop() > 91)
+		{
+			header.addClass('scrolled');
+		}
+		else
+		{
+			header.removeClass('scrolled');
+		}
+	}
+
+	/* 
+
+	3. Init Menu
+
+	*/
+
+	function initMenu()
+	{
+		var hamb = $('.hamburger');
+		var menu = $('.menu');
+		var menuOverlay = $('.menu_overlay');
+		var menuClose = $('.menu_close_container');
+
+		hamb.on('click', function()
+		{
+			menu.toggleClass('active');
+			menuOverlay.toggleClass('active');
+		});
+
+		menuOverlay.on('click', function()
+		{
+			menuOverlay.toggleClass('active');
+			menu.toggleClass('active');
+		});
+
+		menuClose.on('click', function()
+		{
+			menuOverlay.toggleClass('active');
+			menu.toggleClass('active');
+		});
+	}
+
+	/* 
+
+	4. Init Home Slider
+
+	*/
+
+	function initHomeSlider()
+	{
+		if($('.home_slider').length)
+		{
+			var homeSlider = $('.home_slider');
+			homeSlider.owlCarousel(
+			{
+				items:1,
+				autoplay:true,
+				loop:true,
+				nav:false,
+				smartSpeed:1200,
+				mouseDrag:false,
+				dotsContainer:'home_slider_custom_dots'
+			});
+
+			/* Custom dots events */
+			if($('.home_slider_custom_dot').length)
+			{
+				$('.home_slider_custom_dot').on('click', function()
+				{
+					$('.home_slider_custom_dot').removeClass('active');
+					$(this).addClass('active');
+					homeSlider.trigger('to.owl.carousel', [$(this).index(), 300]);
+				});
+			}
+
+			/* Change active class for dots when slide changes by nav or touch */
+			homeSlider.on('changed.owl.carousel', function(event)
+			{
+				$('.home_slider_custom_dot').removeClass('active');
+				$('.home_slider_custom_dots li').eq(event.page.index).addClass('active');
+			});
+		}
+	}
+
+
+	/* 
+
+	5. Init Testimonials Slider
+
+	*/
+
+	function initTestSlider()
+	{
+		if($('.test_slider').length)
+		{
+			var testSlider = $('.test_slider');
+			testSlider.owlCarousel(
+			{
+				items:1,
+				autoplay:false,
+				loop:true,
+				dots:true,
+				smartSpeed:1200
+			});
+		}
+	}
+
+});
+
